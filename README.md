40d28da05ca64d1bccadb35fd8008add085e341f
Lucas Rennó Kallás <lucasrk@unifei.edu.br>
2020-08-02 18:58:24 -0300

    While of date

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # Wordlist With R
 I created this repository to understand R syntax and make a wordlist with it.
 
+Don't forget to install the packages first.
+
 ## The wordlist
 The wordlist will be a set of dates in a interval. The interval will be put by the user.
 
 ## References
 I used the information of [this](http://www.sthda.com/english/wiki/writing-data-from-r-to-txt-csv-files-r-base-functions) site to generate the text file.
 Take a look on it, this web-site show how to create CSV files too.
+
+I used the information of [this](http://material.curso-r.com/lubridate/) site to manipulate the dates. Take a look on it too, this web-site show a lot of interesting functions.
 
 ## R language
 Is very good to use this language to organize and analise data. Maybe I will use it more after that. There are a lot of functions to take Data Science "more seriously".
--- wordlist.R
+++ wordlist.R
@@ -1,9 +1,39 @@
-# This program will generate a .txt file with a list of dates
+# This program will generate a text file with a list of dates
+#       Author: Lucas Rennó Kallás
+#       -- WORDLIST WITH R --
+#       The Wordlist Will be Formated with: DMY or MDY
 #
-#       WORDLIST WITH R
-#
-################################################
 
-dates = c(10,20,30,40,50,60)
+# Install some packages to use the Lubridate Library
+install.packages("tidyverse")
+install.packages("lubridate")
+install.packages("devtools")
+
+# Library Lubridate to manipulate Dates
+library(lubridate)
+
+# Read Values
+begin_date_str <- readline(prompt="Enter the First Date")
+'10/04/2020'
+
+end_date_str <- readline(prompt="Enter the Last Date")
+'10/04/2021'
+
+date1 = dmy(begin_date_str)
+date2 = dmy(end_date_str)
+
+inter <- interval(date1, date2)
+inter2 <- interval(date1,date1)
+int_overlaps(inter, inter2)
+
+dates <- c()
+date_aux = date1
+print(inter == '10/04/2020')
+while(int_overlaps(inter, inter2)){
+    inter2 <- interval(date_aux, date_aux)
+    date_aux <- date_aux+ddays(1)
+    print(date_aux)
+    dates[length(dates)+1] <- date_aux
+}
 
 write.table(dates, file = "wordlist.txt", sep="\n", row.names=F, col.names=F)
